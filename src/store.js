64db04ec5f4fb8c14d9570ee26f822f70d65e72d
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-26 15:12:44 -0400

    Important commit; av support

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+
 
 export default configureStore({
   reducer: {
